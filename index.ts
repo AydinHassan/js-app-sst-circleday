21bba412a03cc20f3b76f0fbc6b2c83390278f01
Flo Hessling <f.hessling@shopware.com>
2024-08-30 13:25:49 +0200

    feat: add datadog layer and extension to lambda function

--- index.ts
+++ index.ts
@@ -3,15 +3,22 @@ import { handle } from 'hono/aws-lambda';
 import { configureAppServer } from '@shopware-ag/app-server-sdk-hono';
 import { DynamoDBRepository } from '@shopware-ag/app-server-sdk-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-
 import type {
     AppServer,
     Context,
     ShopInterface,
 } from '@shopware-ag/app-server-sdk';
+import { createNotificationResponse } from '@shopware-ag/app-server-sdk/helper/app-actions';
+
+declare module "hono" {
+    interface ContextVariableMap {
+        app: AppServer;
+        shop: ShopInterface;
+        context: Context;
+    }
+}
 
 const app = new Hono()
-
 const client = new DynamoDBClient();
 
 configureAppServer(app, {
@@ -22,14 +29,16 @@ configureAppServer(app, {
 
 const name = process.env.NAME
 
-declare module "hono" {
-    interface ContextVariableMap {
-        app: AppServer;
-        shop: ShopInterface;
-        context: Context;
-    }
-}
-
 app.get("/", c => c.text(`hello ${name}.`))
+
+app.post('/app/hello', async (ctx) => {
+    const shop = ctx.get('shop');
+    console.log(shop.getShopUrl());
+
+    const sclient = ctx.get('context');
+    console.log(await sclient.httpClient.get('/_info/version'));
+
+    return createNotificationResponse('success', `hello ${name}.`)
+})
 
 export const handler = handle(app)
--- sst.config.ts
+++ sst.config.ts
@@ -6,45 +6,65 @@ export default $config({
             name: "serverless-hello-world",
             removal: input?.stage === "production" ? "retain" : "remove",
             home: "aws",
+            providers: { datadog: true },
         };
     },
+
     async run() {
         const hostedZone = await aws.route53.getZone({
             name: "apps.shopware.io",
             privateZone: false,
-        })
+        });
+
+        // const ddApiKeySecret = await aws.secretsmanager
+        //     .getSecret({
+        //         name: "production-datadog-api-key",
+        //     });
 
         const table = new sst.aws.Dynamo("shop", {
             fields: {
-                id: "string"
+                id: "string",
             },
-            primaryIndex: { hashKey: "id" }
+            primaryIndex: { hashKey: "id" },
         });
 
         const hono = new sst.aws.Function("hono", {
             url: true,
             link: [table],
+            // layers: [
+            //     "arn:aws:lambda:eu-central-1:464622532012:layer:Datadog-Node20-x:115",
+            //     "arn:aws:lambda:eu-central-1:464622532012:layer:Datadog-Extension:64"
+            // ],
             handler: "index.handler",
             environment: {
-                NAME: 'friend'
-            }
+                // DD_LAMBDA_HANDLER: "index.handler",
+                // DD_SITE: "datadoghq.eu",
+                // DD_API_KEY_SECRET_ARN: ddApiKeySecret.arn,
+                NAME: "friend",
+                APP_NAME: "ServerlessHelloWorldExample",
+                APP_SECRET: "ServerlessHelloWorldExampleSecret",
+            },
         });
 
         const router = new sst.aws.Router("hello-world", {
             routes: {
-                "/*": hono.url
+                "/*": hono.url,
             },
             domain: {
-                name: $app.stage === "production" ? `${$app.name}.${hostedZone.name}` : `${$app.name}-${$app.stage}.${hostedZone.name}`,
+                name:
+                    $app.stage === "production"
+                        ? `${$app.name}.${hostedZone.name}`
+                        : `${$app.name}-${$app.stage}.${hostedZone.name}`,
                 dns: sst.aws.dns({
-                    zone: hostedZone.zoneId
-                })
-            }
-        })
+                    zone: hostedZone.zoneId,
+                }),
+            },
+        });
 
         return {
             api: hono.url,
             router: router.url,
-        }
+        };
+
     },
 });
